1ae255efc13f5a898bc5c19e2cf6bdc1d365f59c
LitvinenkoBD <boris12lit@gmail.com>
2022-08-27 02:11:13 +0300

    added new structure for DeprClassCompo.tsx

--- src/components/DeprClassCompo.tsx
+++ src/components/DeprClassCompo.tsx
@@ -43,14 +43,20 @@ class DeprClassCompo extends Component<IDeprProps, IDeprState> {
     }
 
     render() {
+        console.log('rerender')
         const {testProps} = this.props
         const {inputDataText, num, description, inputDataNum} = this.state
         return (
             <div style={{textAlign: 'center'}}>
-              <h1 >Hello, I'm Deprecated Class Component! And today we will work with local state...</h1>
-              <p>Our props is - {testProps}</p>            
+              <h1 >Hello, I'm Deprecated Class Component! And today we will work with...</h1>
+              <h2>Props</h2>
+              <p>Our props is - {testProps}</p>
+
+              <h2>State</h2>            
               <p>Our state is - {num} // {description}</p>
               <button onClick={this.setPlus}>Plus</button>
+              
+              <h2>Form</h2>
               <p>Our input text value is - {inputDataText}</p>
               <p>Our input num value is - {inputDataNum}</p>
               <form>
@@ -62,6 +68,5 @@ class DeprClassCompo extends Component<IDeprProps, IDeprState> {
         )
     }
 } 
-
 
 export default DeprClassCompo;
